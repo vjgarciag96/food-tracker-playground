ac980b842694a886a08bcbe717239e3da364d5ee
vjgarciag96 <vjgarcia@tuenti.com>
2018-12-26 12:48:38 +0100

    Initial Commit

---
+++ FoodTrackerPlayground/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTrackerPlayground
+//
+//  Created by vjgarcia on 26/12/2018.
+//  Copyright © 2018 vjgarciag96. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
